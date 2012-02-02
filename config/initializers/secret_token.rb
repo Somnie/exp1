96c362fd99891a88748a831f8e98d2dc03e06fe1
Byron Ainsworth <therecondite@gmail.com>
2012-02-02 19:09:27 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exp1::Application.config.secret_token = '8641fadf1760b94a160c0607cc651b47415a8f2ad96ecc021b4fbc92dfbd01b24f2b8808cb01e07ba161cbb78dccac73443f3800fd9acf3170383d2a5ca1387d'
